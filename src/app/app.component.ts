dc5e466587671dfa61a7dff959f52a6f9bff030d
paulinaboron <paulinaboron04@gmail.com>
2023-04-06 23:09:01 +0200

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClassicOX';
+  score = 0;
+  maxScore = 10;
+  width = 10;
+  height = 10;
+  board: Array<Array<number>> = [];
+
+  constructor(){
+    this.createBoard(this.width, this.height);
+  }
+
+  createBoard(w: number, h: number){
+    this.board = [];
+    for(let i = 0; i < h; i++){
+      this.board.push(new Array(w).fill(0));
+    }
+  }
+
+  createRange(number: number){
+    return new Array(number).fill(0)
+      .map((n, index) => index + 1);
+  }
+
+  cellClick(event: MouseEvent){
+    console.log('cell clicked', event);
+    if(event.target instanceof HTMLElement){
+      event.target.innerText = 'O';
+    }
+  }
+
+  save(w: string, h: string, s: string){
+    console.log('save', w, h, s);
+    if(w != "") this.width = parseInt(w);
+    if(h != "") this.height = parseInt(h);
+    if(s != "")this.maxScore = parseInt(s);
+
+    this.createBoard(this.width, this.height);
+  }
+
+  restart(){
+    this.score = 0;
+    this.maxScore = 10;
+    this.width = 10;
+    this.height = 10;
+    this.createBoard(this.width, this.height);
+  }
 }
